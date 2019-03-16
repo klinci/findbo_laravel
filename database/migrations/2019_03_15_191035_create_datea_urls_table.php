e3e9a4f361d3cef48b64f97f9cd710f88b840378
Eric Yonathan <eric.gamer84@gmail.com>
2019-03-16 10:15:29 +0700

    add datea migration

--- database/migrations/2019_03_15_191035_create_datea_urls_table.php
+++ database/migrations/2019_03_15_191035_create_datea_urls_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBirchejendommeUrlsTable extends Migration
+class CreateDateaUrlsTable extends Migration
 {
   /**
   * Run the migrations.
@@ -13,7 +13,7 @@ class CreateBirchejendommeUrlsTable extends Migration
   */
   public function up()
   {
-    Schema::create('birchejendomme_urls', function (Blueprint $table) {
+    Schema::create('datea_urls', function (Blueprint $table) {
       $table->increments('id');
       $table->text('property_url');
       $table->integer('is_synced');
@@ -28,6 +28,6 @@ class CreateBirchejendommeUrlsTable extends Migration
   */
   public function down()
   {
-  	Schema::dropIfExists('birchejendomme_urls');
+  //
   }
 }
